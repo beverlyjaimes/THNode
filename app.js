c744997325181fc027a3230524d8db7a3864cf53
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-04-01 22:43:09 -0500

    tested printMessage function

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ console.log("Hello World!");
 console.error("Something went wrong");
 // for objects 
 console.dir({name:"Bev", age:26});
+
+// Problem: Need a simple way to look at a user's badge count and JS points
+
+// print msg to console
+function printMessage(username, badgeCount, point){
+  const message =  `${username} has ${badgeCount} total badge(s) and $(points) points in javaScript `;
+  console.log(message);
+}
+
+printMessage("bev", 200, 23342);
+
+// Solution: Use Node.js to connect to TH API to get profile information to pring out
+
+// Connect to API URL (https://teamtreehouse.com/beverlyjaimespuente.json)
+// Read data
+// Parse data
+// Print data
